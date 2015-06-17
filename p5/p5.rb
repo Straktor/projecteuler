fb8647e9efe790f3ffcf79f00534dea4eb0731b4
straktor <aboulay56@gmail.com>
2015-06-17 17:09:48 -0400

    updated to start from 1

--- p5/p5.rb
+++ p5/p5.rb
@@ -1,15 +1,16 @@
 #!/usr/bin/ruby
 
-a = 0
-n = 2520
-d = 11
+increment = 1
+number = 1
+divider = 1
+numberOfDivider = 20
 
-while d <= 20
-	a = n
-	while n % d != 0
-		n += a
+while divider <= numberOfDivider
+	increment = number
+	while number % divider != 0
+		number += increment
 	end
-	d += 1
+	divider += 1
 end
 
-puts n
+puts number

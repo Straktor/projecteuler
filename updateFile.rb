b14f40bc0b5038bb397227a2b0c9b59ff1544736
straktor <aboulay56@gmail.com>
2015-07-07 10:40:58 -0400

    updated

--- updateFile.rb
+++ updateFile.rb
@@ -8,33 +8,38 @@ end
 def createPost(mdFile, codeFile)
 	markdownContent = getFileContent(mdFile)
 	codeContent = getFileContent(codeFile)
-	
+
 	# Get the title from the first line ex: Problem 9
-	title =  markdownContent.lines.first.scan(/\w+\s*\d+/).first
+	title = markdownContent.lines.first.scan(/\w+\s*\d+/).first
 	formatedTitle = title.downcase.gsub(/\s+/, "")
 
-	# Add useless date in file name to respect jekyll post format
-	file = File.open("_posts/1111-11-11-" + formatedTitle + ".md", 'w')
+	# Get index
+	index = markdownContent.lines.first.scan(/\d+/).first
 
-	# YAML Header
-	file.write("---\n")
-	file.write("layout: post\n")
-	file.write("title: " + title + "\n")
-	file.write("permalink: " + formatedTitle + "\n")
-	file.write("---\n")
-	
-	# Markdown
-	file.write(markdownContent + "\n")
-	
-	# Code
-	file.write("{% highlight ruby %}\n")
-	file.write(codeContent.gsub(/^$\n/, ''))
-	file.write("{% endhighlight %}\n")
+	# Create post file
+	if !title.empty? and !index.empty?
+		# Add useless date in file name to respect jekyll post format
+		file = File.open("_posts/1111-11-11-" + formatedTitle + ".md", 'w')
 
-	file.close
+		# YAML Header
+		file.write("---\n")
+		file.write("layout: post\n")
+		file.write("title: " + title + "\n")
+		file.write("permalink: " + formatedTitle + "\n")
+		file.write("index: " + index + "\n");
+		file.write("---\n")
+
+		# Markdown
+		file.write(markdownContent + "\n")
+
+		# Code
+		file.write("{% highlight ruby %}\n")
+		file.write(codeContent.gsub(/^$\n/, ''))
+		file.write("{% endhighlight %}\n")
+
+		file.close
+	end
 end
-
-# createPost("problems/p01/p1.md" , "problems/p01/p1.rb")
 
 dir = 'problems/'
 
@@ -42,18 +47,13 @@ subDirectoryList = Dir.entries(dir).select {
 	|entry| File.directory? File.join(dir,entry) and !(entry =='.' || entry == '..')
 }
 
-p subDirectoryList
-
 subDirectoryList.each do |currentSubDir|
-	
 	fullMDfilePath = ""
 	fullCodeFilePath = ""
 
 	files = Dir.entries(dir + currentSubDir).select {|f| !File.directory? f}
-	p files
-	
+
 	files.each do |currentFile|
-	
 		if currentFile =~ /md$/
 			fullMDfilePath = dir + currentSubDir + "/" + currentFile	
 		end
@@ -61,11 +61,9 @@ subDirectoryList.each do |currentSubDir|
 		if currentFile =~ /rb$/
 			fullCodeFilePath = dir + currentSubDir + "/" + currentFile			
 		end
-
 	end
 
 	if !fullMDfilePath.empty? and !fullCodeFilePath.empty?
 		createPost(fullMDfilePath, fullCodeFilePath)	
 	end
-
 end
